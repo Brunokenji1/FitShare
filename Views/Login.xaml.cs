17e9264b6d4600d9d3a7d54176c2a7f738fe69f6
Pedro <pedroa.chaves10@gmail.com>
2025-10-31 17:13:05 -0300

    home

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -9,41 +9,6 @@ public partial class Login : ContentPage
     {
         InitializeComponent();
     }
-    private async void ValidarLogin(object sender, EventArgs e)
-    {
-        try
-        {
-            var usuariosCadastrados = RepositorioUsuarios.ListarTodos();
-
-            string identificador = txt_usuario.Text?.Trim();
-            string senha = txt_senha.Text?.Trim();
-
-            if(string.IsNullOrWhiteSpace(identificador) || string.IsNullOrWhiteSpace(senha))
-            {
-                throw new Exception("Preencha todos os campos!");
-            }
-            var usuarioEncontrado = usuariosCadastrados.FirstOrDefault(usuario =>
-                ((usuario.Username?.Equals(identificador, StringComparison.OrdinalIgnoreCase) == true) ||
-                (usuario.Email?.Equals(identificador, StringComparison.OrdinalIgnoreCase) == true ))
-                && usuario.Senha == senha);
-
-            if (usuarioEncontrado != null)
-            {
-                //await SecureStorage.Default.SetAsync("usuario_logado", usuarioEncontrado.Nome);
-                RepositorioUsuarios.LogarUsuario(usuarioEncontrado);
-                App.Current.MainPage = new NavigationPage(new TabbedPageMenu());
-            }
-            else
-            {
-                throw new Exception("Usuário ou senha inválidos!");
-            }
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Ops...", ex.Message, "Fechar");
-        }
-    }
-
     private async void OnTapCadastrar(object sender, TappedEventArgs e)
     {
         await Navigation.PushAsync(new BasicCadastro());
@@ -63,7 +28,7 @@ public partial class Login : ContentPage
     {
         txt_senha.IsPassword = !txt_senha.IsPassword;
         var imagebButton = (ImageButton)sender;
-        if(txt_senha.IsPassword)
+        if (txt_senha.IsPassword)
         {
             imagebButton.Source = "eye_closed.png";
         }
