b158df6d02b78465919ae70df4a8f5786f4511c0
Pedro <pedroa.chaves10@gmail.com>
2025-10-31 17:16:12 -0300

    Login

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -9,6 +9,41 @@ public partial class Login : ContentPage
     {
         InitializeComponent();
     }
+    private async void ValidarLogin(object sender, EventArgs e)
+    {
+        try
+        {
+            var usuariosCadastrados = RepositorioUsuarios.ListarTodos();
+
+            string identificador = txt_usuario.Text?.Trim();
+            string senha = txt_senha.Text?.Trim();
+
+            if (string.IsNullOrWhiteSpace(identificador) || string.IsNullOrWhiteSpace(senha))
+            {
+                throw new Exception("Preencha todos os campos!");
+            }
+            var usuarioEncontrado = usuariosCadastrados.FirstOrDefault(usuario =>
+                ((usuario.Username?.Equals(identificador, StringComparison.OrdinalIgnoreCase) == true) ||
+                (usuario.Email?.Equals(identificador, StringComparison.OrdinalIgnoreCase) == true))
+                && usuario.Senha == senha);
+
+            if (usuarioEncontrado != null)
+            {
+                //await SecureStorage.Default.SetAsync("usuario_logado", usuarioEncontrado.Nome);
+                RepositorioUsuarios.LogarUsuario(usuarioEncontrado);
+                App.Current.MainPage = new NavigationPage(new TabbedPageMenu());
+            }
+            else
+            {
+                throw new Exception("Usuário ou senha inválidos!");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops...", ex.Message, "Fechar");
+        }
+    }
+
     private async void OnTapCadastrar(object sender, TappedEventArgs e)
     {
         await Navigation.PushAsync(new BasicCadastro());
