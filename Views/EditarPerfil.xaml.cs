19b204f1bc5c164696278e2c274c7ef669c4c483
Bruno Okamoto <obrunokenji@gmail.com>
2025-11-17 00:27:05 -0300

    configurando tela de perfil

--- Views/EditarPerfil.xaml.cs
+++ Views/EditarPerfil.xaml.cs
@@ -1,3 +1,5 @@
+using AppFitShare.Repositories;
+
 namespace AppFitShare.Views;
 
 public partial class EditarPerfil : ContentPage
@@ -5,5 +7,83 @@ public partial class EditarPerfil : ContentPage
 	public EditarPerfil()
 	{
 		InitializeComponent();
-	}
+        RepositorioUsuarios.IniciarUsuarioTemp();
+    }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        var usuario = RepositorioUsuarios.ObterUsuarioTemp();
+
+        if (usuario.Idade != null)
+        {
+            txt_idade.Text = $"{usuario.Idade}";
+        }
+        if (usuario.Altura != null)
+        {
+            txt_altura.Text = $"{usuario.Altura}";
+        }
+        if (usuario.Peso != null)
+        {
+            txt_peso.Text = $"{usuario.Peso}";
+        }
+
+        imgPerfil.Source = usuario.FotoPerfil;
+    }
+    private async void BtnContinuar(object sender, EventArgs e)
+    {
+        try
+        {
+            var usuarioTemp = RepositorioUsuarios.ObterUsuarioTemp();
+
+
+            if (string.IsNullOrWhiteSpace(txt_idade.Text) ||
+               string.IsNullOrWhiteSpace(txt_peso.Text) ||
+               string.IsNullOrWhiteSpace(txt_altura.Text))
+            {
+                throw new Exception("Preencha todos os campos!");
+            }
+            usuarioTemp.Altura = double.Parse(txt_altura.Text);
+            usuarioTemp.Idade = int.Parse(txt_idade.Text);
+            usuarioTemp.Peso = double.Parse(txt_peso.Text);
+            RepositorioUsuarios.AtualizarUsuarioTemp(usuarioTemp);
+            bool confirmacao = await DisplayAlert("Confirma a edição dos seguintes dados?", $"  Altura : {usuarioTemp.Altura}\n  Peso : {usuarioTemp.Peso}\n" +
+    $"  Idade : {usuarioTemp.Idade}  ", "Sim", "Não");
+            if (confirmacao)
+            {
+                usuarioTemp.Status = "Ativo";
+                RepositorioUsuarios.AtualizarUsuarioTemp(usuarioTemp);
+                RepositorioUsuarios.SalvarUsuarioTemp();
+
+                await DisplayAlert("Sucesso", "Cadastro das informações realizado com sucesso!", "Fechar");
+                App.Current.MainPage = new NavigationPage(new TabbedPageMenu());
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops...", ex.Message, "Fechar");
+        }
+
+    }
+
+    private async void BtnSelecionarFoto(object sender, EventArgs e)
+    {
+        try
+        {
+            var foto = await MediaPicker.PickPhotoAsync();
+
+            if (foto == null) return;
+
+            var stream = await foto.OpenReadAsync();
+            var imgSource = ImageSource.FromStream(() => stream);
+
+            imgPerfil.Source = imgSource;
+            imgPerfil.IsVisible = true;
+            var usuario = RepositorioUsuarios.ObterUsuarioLogado();
+            usuario.FotoPerfil = imgSource;
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
 }
--- Views/Perfil.xaml.cs
+++ Views/Perfil.xaml.cs
@@ -10,19 +10,6 @@ public partial class Perfil : ContentPage
 	{
 		InitializeComponent();
     }
-    private async void btnTapSair(object sender, EventArgs e)
-    {
-        var label = (Label)sender;
-        label.IsEnabled = false;
-
-        bool confirmacao = await DisplayAlert("Confirmação", "Deseja realmente sair da sua conta?", "Sim", "Não");
-        if (confirmacao)
-        {
-            RepositorioUsuarios.DeslogarUsuario();
-            App.Current.MainPage = new NavigationPage(new Login());
-        }
-        label.IsEnabled = true;
-    }
 
     private async void BtnCadastrarDados(object sender, EventArgs e)
     {
@@ -32,21 +19,30 @@ public partial class Perfil : ContentPage
     protected override void OnAppearing()
     {
         base.OnAppearing();
-
         var usuario = RepositorioUsuarios.ObterUsuarioLogado();
-        
+
+        lbl_username.Text = usuario.Username;
         lbl_nome.Text = usuario.Nome;
-        if(usuario.Peso != 0 && usuario.Altura != 0)
+        if(usuario.Idade != null)
         {
-            lbl_altura.Text = $"Altura: {usuario.Altura} m";
-            lbl_peso.Text = $"Peso: {usuario.Peso} kg";
-            lbl_nivelAtividade.Text = $"Nível de Atividade: {usuario.NivelAtividade}";
-            lbl_idade.Text = $"Idade: {usuario.Idade} anos";
-            lbl_objetivo.Text = $"Objetivo: {usuario.Objetivo1}";
-            lbl_telefone.Text = $"Telefone: {usuario.Telefone}";
+            lbl_idade.Text = $"Idade: {usuario.Idade}";
+            lbl_idade.IsVisible = true;
         }
-        
-        
+        if(usuario.Telefone != null)
+        {
+            lbl_telefone.Text = $"Telefone: {usuario.Telefone}";
+            lbl_telefone.IsVisible = true;
+        }
+        if(usuario.Altura != null)
+        {
+            lbl_altura.Text = $"Altura: {usuario.Altura}cm";
+            lbl_altura.IsVisible = true;
+        }
+        if(usuario.Peso != null)
+        {
+            lbl_peso.Text = $"Peso: {usuario.Peso}kg";
+            lbl_peso.IsVisible = true;
+        }
         
         imgPerfil.Source = usuario.FotoPerfil;
     }
@@ -54,5 +50,15 @@ public partial class Perfil : ContentPage
     private async void BtnEditarPerfil(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new EditarPerfil());
+    }
+
+    private async void BtnSair(object sender, EventArgs e)
+    {
+        bool confirmacao = await DisplayAlert("Confirmação", "Deseja realmente sair da sua conta?", "Sim", "Não");
+        if (confirmacao)
+        {
+            RepositorioUsuarios.DeslogarUsuario();
+            App.Current.MainPage = new NavigationPage(new Login());
+        }
     }
 }
