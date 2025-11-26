48050bd4b7ee291868ae7da015e2d102e409d5e3
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-11-26 11:10:59 -0300

    att telas de treino

--- Views/MeusTreinos.xaml.cs
+++ Views/MeusTreinos.xaml.cs
@@ -1,6 +1,5 @@
 using AppFitShare.Models;
 using AppFitShare.Repositories;
-using AppFitShare.Services;
 using AppFitShare.Views.TelasCadastroCompleto;
 using Microsoft.Maui.Controls.PlatformConfiguration;
 using System.Threading.Tasks;
@@ -9,7 +8,6 @@ namespace AppFitShare.Views;
 
 public partial class MeusTreinos : ContentPage
 {
-    private readonly JsonStorageService storageService = new();
     private List<Treino> treinos = new();
 
     public MeusTreinos()
@@ -24,25 +22,7 @@ public partial class MeusTreinos : ContentPage
         _ = CarregarTreinosAsync();
     }
     private async Task CarregarTreinosAsync()
-    {
-        treinos = await storageService.LoadAsync<Treino>("treinos.json");
-        if (treinos.Count == 0)
-        {
-            treinos = new List<Treino>
-            {
-                new Treino
-                {
-                    Id = 1, Nome = "Treino A",
-                    ListaDeExercicios = new List<Exercicio>
-                    {
-                        new Exercicio{ Nome="Supino reto", Series=4, Repeticoes=10},
-                        new Exercicio{ Nome="Crucifixo", Series=3, Repeticoes=12}
-                    }
-                }
-            };
-            await storageService.SaveAsync("treinos.json", treinos);
-        }
-    }
+    { }
     private async void BotaoIniciarTreinoClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new ExerciciosTreinos());
@@ -56,5 +36,6 @@ public partial class MeusTreinos : ContentPage
     private async void BtnCadastroSaude(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new CadastrarDados());
-    }
+    } 
 }
+
